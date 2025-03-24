76d08e9f344ba76879d84123de81ab321b362abc
Tanmaya Kumar Sahni <64470892+tanmaya48@users.noreply.github.com>
2025-03-24 21:05:41 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,10 +1,4 @@
-import os
-prof_id = os.environ.get('PROF_ID')
+profile_images_dir=f'../aftershoot/Profile_1/TIFFs/'
+sliders_table_path=f'../aftershoot/Profile_1/sliders_exif.csv'
 
 
-profile_images_dir=f'../aftershoot/Profile_{prof_id}/TIFFs/'
-sliders_table_path=f'../aftershoot/Profile_{prof_id}/sliders_exif.csv'
-
-
-if __name__ == '__main__':
-    print(prof_id)
